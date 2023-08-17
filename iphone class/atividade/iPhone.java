93267b21552032fbc1d2ce950743c8382d2b3dae
Davi Roberto Santos da Silva <drss.davi@gmail.com>
2023-08-17 13:48:46 -0300

    atividade

---
+++ iphone class/atividade/iPhone.java
@@ -0,0 +1,40 @@
+public class iPhone implements ReprodutorMusical, AparelhoTelefonico, NavegadorInternet {
+    // Implementação dos métodos da interface ReprodutorMusical
+    public void tocar() {
+        // Implementação
+    }
+
+    public void pausar() {
+        // Implementação
+    }
+
+    public void selecionarMusica(String musica) {
+        // Implementação
+    }
+
+    // Implementação dos métodos da interface AparelhoTelefonico
+    public void ligar() {
+        // Implementação
+    }
+
+    public void atender() {
+        // Implementação
+    }
+
+    public void iniciarCorreioVoz() {
+        // Implementação
+    }
+
+    // Implementação dos métodos da interface NavegadorInternet
+    public void exibirPagina(String pagina) {
+        // Implementação
+    }
+
+    public void adicionarNovaAba(String url) {
+        // Implementação
+    }
+
+    public void atualizarPagina() {
+        // Implementação
+    }
+}
