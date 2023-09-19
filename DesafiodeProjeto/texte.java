c5c336790368a2914d42be56210d7089dd908c76
Davi Roberto Santos da Silva <drss.davi@gmail.com>
2023-09-19 17:51:51 -0300

    texte

---
+++ DesafiodeProjeto/texte.java
@@ -0,0 +1,10 @@
+package DesafiodeProjeto;
+
+public class texte {
+    public static void main(String[]args) {
+        SingletonLazy lazy= SingletonLazy.getInstancia();
+        System.out.println(lazy);
+        lazy = SingletonLazy.getInstancia();
+        System.out.println(lazy);
+    }
+}
